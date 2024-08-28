e36e18319cae82d50420768bd1efcc58dc6b710f
Fernando Saraiva <fpsaraiva@gmail.com>
2024-08-28 20:14:16 -0300

    customized inputs

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,36 +1,54 @@
-import { Play } from 'phosphor-react'
+import { Play } from "phosphor-react";
 
-import styles from './Index.module.css'
+import styles from "./Index.module.css";
 
 function Home() {
-    return (
-        <main className={styles.homeContainer}>
-            <form action="">
-                <div className={styles.formContainer}>
-                    <label htmlFor="task">Vou trabalhar em</label>
-                    <input type="task" />
+  return (
+    <main className={styles.homeContainer}>
+      <form action="">
+        <div className={styles.formContainer}>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input
+            className={`${styles.baseInput} ${styles.taskInput}`}
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
 
-                    <label htmlFor="minutesAmount">durante</label>
-                    <input type="number" id="minutesAmount"/>
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
-                    <span>minutos.</span>
-                </div>
+          <label htmlFor="minutesAmount">durante</label>
+          <input
+            className={`${styles.baseInput} ${styles.minutesAmountInput}`}
+            type="number"
+            id="minutesAmount"
+            min={1}
+            max={60}
+          />
 
-                <div className={styles.countdownContainer}>
-                    <span>0</span>
-                    <span>0</span>
-                    <div>:</div>
-                    <span>0</span>
-                    <span>0</span>
-                </div>
+          <span>minutos.</span>
+        </div>
 
-                <button type="button">
-                    <Play size={24} />
-                    Começar
-                </button>
-            </form>
-        </main>
-    );
-};
+        <div className={styles.countdownContainer}>
+          <span>0</span>
+          <span>0</span>
+          <div>:</div>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="button" className={styles.buttonContainer}>
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </main>
+  );
+}
 
 export default Home;
