0c61cf017631cace1b980b9095098215d1ac6811
Fernando Saraiva <fpsaraiva@gmail.com>
2024-08-29 00:16:00 -0300

    added status component

--- src/pages/History/Index.tsx
+++ src/pages/History/Index.tsx
@@ -20,37 +20,63 @@ function History() {
               <td>Tarefa</td>
               <td>28 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <span className={`${styles.status} ${styles.statusColorGreen}`}>
+                  Concluído
+                </span>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>28 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <span className={`${styles.status} ${styles.statusColorGreen}`}>
+                  Concluído
+                </span>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>28 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <span
+                  className={`${styles.status} ${styles.statusColorYellow}`}
+                >
+                  Concluído
+                </span>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>28 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <span className={`${styles.status} ${styles.statusColorGreen}`}>
+                  Concluído
+                </span>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>28 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <span className={`${styles.status} ${styles.statusColorGreen}`}>
+                  Concluído
+                </span>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>28 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <span className={`${styles.status} ${styles.statusColorRed}`}>
+                  Concluído
+                </span>
+              </td>
             </tr>
           </tbody>
         </table>
